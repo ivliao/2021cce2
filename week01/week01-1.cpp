b64b1fb152fc26d01c5401c1c626c1c925dd1708
廖云瑄 <91170820+ivliao@users.noreply.github.com>
2022-02-24 17:26:04 +0800

    Add files via upload

---
+++ week01/week01-1.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("使用者輸入一個數字:");
+    scanf("%d",&a);
+    printf("你讀到了 %d\n",a);
+}
---
+++ week01/week01-5.cpp
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <string.h>
+char line[2000];
+int main()
+{
